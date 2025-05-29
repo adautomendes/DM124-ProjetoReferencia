b0f17f600aee02d008064d71d34ebb2e35763a7d
Adauto Mendes <adauto.inatel@gmail.com>
2025-05-29 10:28:29 -0300

    Minor fixes

--- monitor/src/controllers/AlarmeController.js
+++ monitor/src/controllers/AlarmeController.js
@@ -1,4 +1,4 @@
-const alarmeList = {
+const alarmeMap = {
     "DB_0001": {
         id: 'DB_0001',
         descricao: 'MongoDB fora do ar.',
@@ -12,34 +12,34 @@ module.exports = {
         const alarmeId = req.params.id;
 
         // Verificando se o alarme existe
-        if (!alarmeList[alarmeId]) {
+        if (!alarmeMap[alarmeId]) {
             console.log(`Alarme ${alarmeId} não encontrado.`);
             return res.status(404).json({ msg: `Alarme não encontrado.` });
         }
 
         // Ativando o alarme
-        alarmeList[alarmeId].ativo = true;
-        alarmeList[alarmeId].ativacoes.push(new Date());
+        alarmeMap[alarmeId].ativo = true;
+        alarmeMap[alarmeId].ativacoes.push(new Date());
 
         console.log(`Alarme ${alarmeId} ativado!`);
-        return res.status(200).json(alarmeList[alarmeId]);
+        return res.status(200).json(alarmeMap[alarmeId]);
     },
 
     async desativar(req, res) {
         const alarmeId = req.params.id;
 
         // Verificando se o alarme existe
-        if (!alarmeList[alarmeId]) {
+        if (!alarmeMap[alarmeId]) {
             console.log(`Alarme ${alarmeId} não encontrado.`);
             return res.status(404).json({ msg: `Alarme não encontrado.` });
         }
 
         // Desativando o alarme
-        alarmeList[alarmeId].ativo = false;
-        alarmeList[alarmeId].ativacoes = [];
+        alarmeMap[alarmeId].ativo = false;
+        alarmeMap[alarmeId].ativacoes = [];
 
         console.log(`Alarme ${alarmeId} desativado!`);
-        return res.status(200).json(alarmeList[alarmeId]);
+        return res.status(200).json(alarmeMap[alarmeId]);
     },
 
     async buscar(req, res) {
@@ -47,12 +47,12 @@ module.exports = {
 
         // Se a query ativado for fornecida, filtra os alarmes
         if (ativoQuery) {
-            const list = Object.values(alarmeList).filter(alarme => {
+            const list = Object.values(alarmeMap).filter(alarme => {
                 return String(alarme.ativo) === ativoQuery;
             });
             return res.status(200).json(list);
         }
         // Se não, retorna todos os alarmes
-        return res.status(200).json(Object.values(alarmeList));
+        return res.status(200).json(Object.values(alarmeMap));
     },
 };
